b14ef35602a6d4dc783978f216036848e351e465
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-05-25 15:18:42 +0500

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+let son = Number(prompt("Biror soni kiriting"));
+let taxminam = Math.floor(Math.random () * 100); 
+
+alert(`${taxminam} + ${son} = ${taxminam + son}`);
+alert(`${taxminam} - ${son} = ${taxminam - son}`);
+alert(`${taxminam} * ${son} = ${taxminam * son}`);
+alert(`${taxminam} / ${son} = ${taxminam / son}`);
+alert(`${taxminam} % ${son} = ${taxminam % son}`);
